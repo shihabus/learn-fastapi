33217e2e9ec4b8fd776fd97f21e765b3b482b42c
Shihabudheen US <sus@salesforce.com>
2023-07-30 00:27:34 +0530

    added sqlAlchemy

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from random import randrange
 import time
 from typing import Optional
-from fastapi import FastAPI, Response, status, HTTPException
+from fastapi import FastAPI, Response, status, HTTPException, Depends
 from fastapi.params import Body
 
 
@@ -10,6 +10,15 @@ from pydantic import BaseModel
 
 import psycopg2
 from psycopg2.extras import RealDictCursor
+
+# start using sqlalchemy
+from . import models
+from .database import engine, get_db
+from sqlalchemy.orm import Session
+
+
+# connect to DB
+models.Base.metadata.create_all(bind=engine)
 
 
 # connect to db using psycopg2
@@ -39,7 +48,7 @@ class Post(BaseModel):
     content: str
     # optional and defaulted to True
     published: bool = True
-    rating: Optional[int] = None
+    # rating: Optional[int] = None
 
 
 # temp, store posts in memory
@@ -66,39 +75,52 @@ async def root():
 
 
 @app.get("/posts")
-async def get_posts():
-    cursor.execute("""SELECT * FROM posts""")
-    posts = cursor.fetchall()
-    print(posts)
+async def get_posts(db: Session = Depends(get_db)):
+    # cursor.execute("""SELECT * FROM posts""")
+    # posts = cursor.fetchall()
+
+    posts = db.query(models.Post).all()
     return {"data": posts}
 
 
 # Create a post route
 # pass default status code
 @app.post("/posts", status_code=status.HTTP_201_CREATED)
-def create_posts(
-    post: Post,
-):
-    # don't do this as it is vulnerable to SQL injection
-    # cursor.execute(f"INSERT INTO posts (title,content,published) VALUES ({post.title}, {post.content}, {post.published})")
+def create_posts(post: Post, db: Session = Depends(get_db)):
+    # # don't do this as it is vulnerable to SQL injection
+    # # cursor.execute(f"INSERT INTO posts (title,content,published) VALUES ({post.title}, {post.content}, {post.published})")
 
-    # the SQL query is sanitized to prevent SQL injection
-    cursor.execute(
-        """INSERT INTO posts (title,content,published) VALUES (%s,%s,%s) RETURNING *""",
-        (post.title, post.content, post.published),
-    )
-    new_post = cursor.fetchone()
-    # commit the changes to save it to the actual DB
-    conn.commit()
+    # # the SQL query is sanitized to prevent SQL injection
+    # cursor.execute(
+    #     """INSERT INTO posts (title,content,published) VALUES (%s,%s,%s) RETURNING *""",
+    #     (post.title, post.content, post.published),
+    # )
+    # new_post = cursor.fetchone()
+    # # commit the changes to save it to the actual DB
+    # conn.commit()
+
+    # new_post = models.Post(
+    #     title=post.title, content=post.content, published=post.published
+    # )
+
+    # spread operator
+    new_post = models.Post(**post.dict())
+    db.add(new_post)
+    db.commit()
+    # similar to RETURNING in SQL
+    db.refresh(new_post)
+
     return {"data": new_post}
 
 
 # id is path parameter
 @app.get("/posts/{id}")
 # we can validate the path params
-def get_post(id: int, response: Response):
-    cursor.execute("""SELECT * FROM posts WHERE id=%s""", str(id))
-    post = cursor.fetchone()
+def get_post(id: int, response: Response, db: Session = Depends(get_db)):
+    # cursor.execute("""SELECT * FROM posts WHERE id=%s""", str(id))
+    # post = cursor.fetchone()
+
+    post = db.query(models.Post).filter(models.Post.id == id).first()
     if not post:
         # Response.status = status.HTTP_404_NOT_FOUND
         # return {"message": f"post with {id} was not found"}
@@ -110,23 +132,38 @@ def get_post(id: int, response: Response):
 
 # delete a post
 @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_post(id: int):
-    cursor.execute("""DELETE FROM posts WHERE id=%s RETURNING *""", str(id))
-    deleted_post = cursor.fetchone()
-    conn.commit()
-    if deleted_post == None:
+def delete_post(id: int, db: Session = Depends(get_db)):
+    # cursor.execute("""DELETE FROM posts WHERE id=%s RETURNING *""", str(id))
+    # deleted_post = cursor.fetchone()
+    # conn.commit()
+
+    post = db.query(models.Post).filter(models.Post.id == id)
+    if post.first() is None:
         raise HTTPException(status.HTTP_404_NOT_FOUND, f"post with {id} was not found")
-    return {"data": deleted_post}
+    post.delete(synchronize_session=False)
+    db.commit()
+    return {"data": "post was deleted"}
 
 
 @app.put("/posts/{id}")
-def update_post(id: int, post: Post):
-    cursor.execute(
-        """UPDATE posts SET title=%s, content=%s, published=%s WHERE id=%s RETURNING *""",
-        (post.title, post.content, post.published, str(id)),
-    )
-    updated_post = cursor.fetchone()
-    conn.commit()
-    if updated_post == None:
+def update_post(id: int, updated_post: Post, db: Session = Depends(get_db)):
+    # cursor.execute(
+    #     """UPDATE posts SET title=%s, content=%s, published=%s WHERE id=%s RETURNING *""",
+    #     (post.title, post.content, post.published, str(id)),
+    # )
+    # updated_post = cursor.fetchone()
+    # conn.commit()
+
+    post_query = db.query(models.Post).filter(models.Post.id == id)
+    post = post_query.first()
+    if post is None:
         raise HTTPException(status.HTTP_404_NOT_FOUND, f"post with {id} was not found")
-    return {"data": updated_post}
+    post_query.update(updated_post.dict(), synchronize_session=False)
+    db.commit()
+    return {"data": post_query.first()}
+
+
+@app.get("/sqlalchemy")
+def test_posts(db: Session = Depends(get_db)):
+    posts = db.query(models.Post).all()
+    return {"data": posts}
