bb6d1d7fefbdafc5ab8e8c03fc2a97ad4e532d6f
Shihabudheen US <sus@salesforce.com>
2023-07-30 14:34:31 +0530

    remove unused imports

--- app/database.py
+++ app/database.py
@@ -10,9 +10,30 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
+
 def get_db():
     db = SessionLocal()
     try:
         yield db
     finally:
         db.close()
+
+
+# NOT USED
+# connect to db using psycopg2
+# while True:
+#     try:
+#         conn = psycopg2.connect(
+#             host="localhost",
+#             dbname="fastapi",
+#             user="postgres",
+#             password="1234",
+#             cursor_factory=RealDictCursor,
+#         )
+#         cursor = conn.cursor()
+#         print("DB connection was successful!")
+#         break
+#     except Exception as error:
+#         print("Connecting to db failed")
+#         print("Error: ", error)
+#         time.sleep(2)
--- app/main.py
+++ app/main.py
@@ -1,9 +1,4 @@
-import time
 from fastapi import FastAPI
-
-
-import psycopg2
-from psycopg2.extras import RealDictCursor
 
 # start using sqlalchemy
 from . import models
@@ -12,46 +7,14 @@ from .database import engine
 # routes
 from .routers import posts, users, auth
 
-
 # connect to DB
 models.Base.metadata.create_all(bind=engine)
 
-
-# connect to db using psycopg2
-while True:
-    try:
-        conn = psycopg2.connect(
-            host="localhost",
-            dbname="fastapi",
-            user="postgres",
-            password="1234",
-            cursor_factory=RealDictCursor,
-        )
-        cursor = conn.cursor()
-        print("DB connection was successful!")
-        break
-    except Exception as error:
-        print("Connecting to db failed")
-        print("Error: ", error)
-        time.sleep(2)
-
 app = FastAPI()
 
-
-# temp, store posts in memory
-my_posts = [{"id": 1, "title": "Title of post 1", "content": "Content of post 1"}]
-
-
-def find_post(id):
-    for p in my_posts:
-        if p["id"] == id:
-            return p
-
-
-def find_post_index(id):
-    for i, p in enumerate(my_posts):
-        if p["id"] == id:
-            return i
+app.include_router(posts.router)
+app.include_router(users.router)
+app.include_router(auth.router)
 
 
 # Path operation or route
@@ -59,13 +22,3 @@ def find_post_index(id):
 # function name can be anything, need not be root
 async def root():
     return {"message": "FASTAPI is running"}
-
-
-app.include_router(posts.router)
-app.include_router(users.router)
-app.include_router(auth.router)
-
-# @app.get("/sqlalchemy")
-# def test_posts(db: Session = Depends(get_db)):
-#     posts = db.query(models.Post).all()
-#     return posts
