3ab4e9b4b873014354161040ccdbf131d2bd051b
Shihabudheen US <sus@salesforce.com>
2023-07-30 19:10:54 +0530

    add a JOIN query in sqlAlchemy

--- app/routers/posts.py
+++ app/routers/posts.py
@@ -2,13 +2,14 @@ from fastapi import Response, status, HTTPException, Depends, APIRouter
 from .. import models, schemas, oauth2
 from ..database import get_db
 from sqlalchemy.orm import Session
+from sqlalchemy import func
 from typing import List, Optional
 
 router = APIRouter(prefix="/posts", tags=["Posts"])
 
 
 # %20 -> space in query param
-@router.get("/", response_model=List[schemas.Post])
+@router.get("/", response_model=List[schemas.PostOut])
 async def get_posts(
     db: Session = Depends(get_db),
     limit: int = 10,
@@ -19,15 +20,25 @@ async def get_posts(
     # posts = cursor.fetchall()
 
     # posts = db.query(models.Post).filter(models.Post.owner_id == current_user.id).all()
-    posts = (
-        db.query(models.Post)
+    # posts = (
+    #     db.query(models.Post)
+    #     .filter(models.Post.title.contains(search))
+    #     .limit(limit)
+    #     .offset(skip)
+    #     .all()
+    # )
+
+    results = (
+        db.query(models.Post, func.count(models.Vote.post_id).label("votes"))
+        .join(models.Vote, models.Post.id == models.Vote.post_id, isouter=True)
+        .group_by(models.Post.id)
         .filter(models.Post.title.contains(search))
         .limit(limit)
         .offset(skip)
         .all()
     )
 
-    return posts
+    return results
 
 
 # Create a post route
@@ -65,13 +76,20 @@ def create_posts(
 
 
 # id is path parameter
-@router.get("/{id}", response_model=schemas.Post)
+@router.get("/{id}", response_model=schemas.PostOut)
 # we can validate the path params
 def get_post(id: int, response: Response, db: Session = Depends(get_db)):
     # cursor.execute("""SELECT * FROM posts WHERE id=%s""", str(id))
     # post = cursor.fetchone()
 
-    post = db.query(models.Post).filter(models.Post.id == id).first()
+    # post = db.query(models.Post).filter(models.Post.id == id).first()
+    post = (
+        db.query(models.Post, func.count(models.Vote.post_id).label("votes"))
+        .join(models.Vote, models.Post.id == models.Vote.post_id, isouter=True)
+        .group_by(models.Post.id)
+        .first()
+    )
+
     if not post:
         # Response.status = status.HTTP_404_NOT_FOUND
         # return {"message": f"post with {id} was not found"}
